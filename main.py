2c376aa4278291e232f785710ffba35bedaf0230
Николай <fogumen@gmail.com>
2021-12-15 11:37:51 +0200

    pattern

--- main.py
+++ main.py
@@ -17,28 +17,7 @@ def Random_field(n):
 
 def Choise():
     # function in which the user selects his playing field
-    great_field = False
-    while great_field == False:
-        users_field = Random_field(0)
-        for i in range(1, 11):
-            for j in range(1, 11):
-                print(users_field[i][j], end=' ')
-            print()
-        print("""Do you agree with this field?
-1 - Yes
-2 - No""")
-        a = input(">>> ")
-        while a != '1' and a != '2':
-            print("Error. Try again")
-            a = input(">>> ")
-        a = int(a)
 
-        if a == 1:
-            great_field = True
-            print("Great!")
-        else:
-            print("Generating a new...")
-    return users_field
 
 def main():
     # main function with gameplay
